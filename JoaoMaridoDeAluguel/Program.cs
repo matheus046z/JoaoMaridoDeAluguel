0cbb4e1482ce3cf6a75b651205967b8ea8739cc3
Matheus Santana <mt.1_@live.com>
2023-04-29 22:12:25 -0300

    Cria dicionario com campos e valores adicionados

--- JoaoMaridoDeAluguel/Program.cs
+++ JoaoMaridoDeAluguel/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text.RegularExpressions;
+using System.Linq.Expressions;
 // 1 linha e a ordem de serviço
 
 namespace JoaoMaridoDeAluguel
@@ -15,82 +16,83 @@ namespace JoaoMaridoDeAluguel
         static void Main(string[] args)
         {
 
-        //    List<string> Campos = new List<string>()
-        //{
-        //    "Solicitação:", "Produto:", "Aceito em:", "Serviço:", "Prazo:", "Local:", "Complemento:", "Bairro:",
-        //    "Região:", "Cidade:", "Estado:", "CEP:", "Referências:", "Problema:", "Importante:", "Segurado:",
-        //    "Solicitante:", "Telefone:", "Destino:", "Local:", "Complemento:", "Referência:", "Bairro:", "Região:",
-        //    "Cidade:", "Estado:", "Equipamento:", "Marca:", "Ano:"
-        //};
+            // Caminho do txt
+            string path = "E:\\CURSOS\\SENAI\\Desafio-txt\\JoaoMaridoDeAluguel\\JoaoMaridoDeAluguel\\bin\\Debug\\dados.txt";
 
-            //int i = 0;
-
-            //while (i < Campos.Count)
-            //{
-            //    Console.WriteLine(Campos[i]);
-            //    i++;
-            //}
-            //Console.ReadKey();
-
-            // Replace "file.txt" with the path to your text file
-            
-            string path = "C:\\Users\\ALUNO\\source\\repos\\Tarefa3\\JoaoMaridoDeAluguel\\dados.txt";
-
-            // Define the list of items to look for in the text file
+            // Lista de campos presentes no arquivo
             List<string> campos = new List<string>()
         {
             "Solicitação:", "Produto:", "Aceito em:", "Serviço:", "Prazo:", "Local:", "Complemento:", "Bairro:",
             "Região:", "Cidade:", "Estado:", "CEP:", "Referências:", "Problema:", "Importante:", "Segurado:",
-            "Solicitante:", "Telefone:", "Destino:", "Local:", "Complemento:", "Referência:", "Bairro:", "Região:",
-            "Cidade:", "Estado:", "Equipamento:", "Marca:", "Ano:"
-        };
+            "Solicitante:", "Telefone:", "Destino:", "Referência:", "Equipamento:", "Marca:", "Ano:"
+        }; // Local, complemento, bairro, reagiao, cidade, estado, referencia/referencias,
 
             List<string> valores;
 
             // Le o txt com codiicação utf 8 e armazena na variavel Dados
-
             string Dados = File.ReadAllText(path, Encoding.UTF8);
             string infoCampo = Dados;
 
-            // Procura os itens da lista campos no texto e substitui por ","
+            // Procura os itens da lista campos no texto e substitui por ";"
             foreach (string campo in campos)
             {
-                infoCampo = infoCampo.Replace(campo, ",");
-                infoCampo = Regex.Replace(infoCampo, campo, ",");
+                infoCampo = infoCampo.Replace(campo, ";");
+                infoCampo = Regex.Replace(infoCampo, campo, ";");
   
             }
 
-            //File.WriteAllText("infoCampos.txt", infoCampo);
-            //Console.WriteLine(infoCampo);
-            //Console.ReadKey();
-            //Console.Clear();
+            // Cria uma lista somente com os valores separados por ";"
+            valores = new List<string>(infoCampo.Split(';'));
 
-            // Cria uma lista somente com os valores separados por ","
-            valores = new List<string>(infoCampo.Split(','));
+            // Descobre onde estão o index de cada item printando no terminal
+            //for (int i = 0; i < valores.Count; i++)
+            //{
+            //    try
+            //    {
+            //        valores[i] = valores[i].Trim();
+            //        
+            //        Console.WriteLine("index de valores: " + i);
+            //        Console.WriteLine("Campo: " + campos[i] + " -  " + valores[i+1]);
+            //    }
+            //    catch (System.ArgumentOutOfRangeException)
+            //    {
+            //        break;
+            //    }
+            //}
 
-            for (int i = 0; i < valores.Count; i++)
+            IDictionary<string, string> DadosDic = new Dictionary<string, string>();
+            DadosDic.Add("OS:", valores[0].Trim());
+
+            IDictionary<string, string> DadosDicRepetidos = new Dictionary<string, string>();
+
+            int c = 0;
+
+            while (c <= campos.Count)
             {
-                valores[i] = valores[i].Trim();
-                // Descobrir onde estão os index de cada item printando no terminal
-                Console.WriteLine(valores[i]);
-                Console.WriteLine("index nr: " + i);
+                try
+                {
+                    DadosDic.Add(campos[c].Trim(), valores[c + 1].Trim());
+                }
+                catch (System.ArgumentException) // O txt tem campos repetidos,  esta tentando sobrescrever item existente no dicionario e dando ArgumentException
+                {
+                    //Console.WriteLine("System.ArgumentException");
+                    try
+                    {
+                        DadosDicRepetidos.Add(campos[c].Trim(), valores[c + 1].Trim());
+                    }
+                    catch (System.ArgumentOutOfRangeException)
+                    {
+                        //Console.WriteLine("ArgumentOutOfRangeException");
+                        break; // encerra o loop quando acaba os valores para adicioanr ao dicionario
+                    }
+                }
+                c = c + 1;
             }
 
+            DadosDic.Select(i => $"{i.Key} {i.Value}").ToList().ForEach(Console.WriteLine);
+            //Console.WriteLine("\nDADOS REPETIDOS: \n");
+            //DadosDicRepetidos.Select(i => $"{i.Key} {i.Value}").ToList().ForEach(Console.WriteLine);
             Console.ReadKey();
-            Console.Clear();
-
-            var pairs = campos.Zip(infoCampo, (k, v) => new { k, v })
-                              .ToDictionary(x => x.k, x => x.v);
-            
-            Console.Write(pairs);
-            Console.ReadKey();
-
-            //foreach (string valor in valores)
-            //{
-            //    Console.WriteLine(valor);
-            //}
-            //Console.ReadKey();
-
         }
     }
 }
@@ -98,6 +100,6 @@ namespace JoaoMaridoDeAluguel
 
 // passos
 // Criar lista com os campos
-// Substituir os campos por "," e criar um arquivo só com os valores dos campos separados por ","
+// Substituir os campos por ";" e criar um arquivo só com os valores dos campos separados por ";"
 // Criar um dicionario relacionando os campos com seus respectivos valores
 
